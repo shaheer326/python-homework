13a472eea8a4fa82758c8180e068615cb332a5a8
shaheer326 <shaheerkhan1211@.com>
2026-07-04 16:44:01 +0500

    submit

---
+++ birthdate.py
@@ -0,0 +1,11 @@
+date1 = "2012-11-12"
+date2 = "2013-05-23"
+date3 = "2011-02-14"
+date4 = "2012-07-08"
+date5 = "2013-12-30"
+
+print(date1)
+print(date2)
+print(date3)
+print(date4)
+print(date5)
---
+++ congratulations.py
@@ -0,0 +1,6 @@
+text = str(input("Enter your text: "))
+
+bigtext = text.upper()
+text = bigtext
+
+print("Your text in uppercase is:", text)
