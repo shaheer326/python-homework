32c7dca97d7e6b7214404869b5b42d2016d357c7
shaheer326 <shaheerkhan1211@.com>
2026-07-12 11:49:48 +0500

    homework

---
+++ cold.py
@@ -0,0 +1,6 @@
+temperature = int(input("enter the temperature (C): "))
+
+if temperature >= 25:
+    print("You can wear light clothes")
+else:
+    print("its too cold u cant wear light clothes")
---
+++ team.py
@@ -0,0 +1,11 @@
+team1 = int(input("enter obtained marks for team 1: "))
+team2 = int(input("enter obtained marks for team 2: "))
+team3 = int(input("enter obtained marks for team 3: "))
+team4 = int(input("enter obtained marks for team 4: "))
+team5 = int(input("enter obtained marks for team 5: "))
+
+sum = team1 + team2 + team3 + team4 + team5
+print("the sum of the marks is:", sum)
+
+average = sum/5
+print("the average of the marks is:", average)
